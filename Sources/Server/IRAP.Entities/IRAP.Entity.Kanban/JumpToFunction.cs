b523a14808f6e0c5d6b5dc1e671bfbf680b7cdb8
zhiying.li <zhiying.li@1351d8d5-24a7-d647-b81a-b5ab284c741b>
2016-02-03 08:10:53 +0000

    新增 Kanban 服务

---
+++ Sources/Server/IRAP.Entities/IRAP.Entity.Kanban/JumpToFunction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRAP.Entity.Kanban
+{
+    public class JumpToFunction
+    {
+        /// <summary>
+        /// 序号
+        /// </summary>
+        public int Ordinal
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 跳转功能叶标识
+        /// </summary>
+        public int JumpToT3LeafID
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 跳转前等待时间（秒）
+        /// </summary>
+        public int WaitForSeconds
+        {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return JumpToT3LeafID.ToString();
+        }
+    }
+}
