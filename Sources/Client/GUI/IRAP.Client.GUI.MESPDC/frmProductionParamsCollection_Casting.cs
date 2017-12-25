4d261a4516854d52f2d28c77eb7cdbca92317f7e
xinpeng.ke <xinpeng.ke@1351d8d5-24a7-d647-b81a-b5ab284c741b>
2017-12-25 05:57:51 +0000

    添加窗体

---
+++ Sources/Client/GUI/IRAP.Client.GUI.MESPDC/frmProductionParamsCollection_Casting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IRAP.Client.GUI.MESPDC {
+    public partial class frmProductionParamsCollection_Casting : IRAP.Client.Global.GUI.frmCustomFuncBase {
+        public frmProductionParamsCollection_Casting() {
+            InitializeComponent();
+        }
+    }
+}
