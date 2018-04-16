922f5dab659b24c70ab85361c15f3b258a5553c1
liangtian.zhou <liangtian.zhou@1351d8d5-24a7-d647-b81a-b5ab284c741b>
2018-04-16 09:52:51 +0000

    新增获取未显示设备数据库接口

--- Sources/Server/IRAP.BL/IRAP.BL.MDM/IRAPMDM.cs
+++ Sources/Server/IRAP.BL/IRAP.BL.MDM/IRAPMDM.cs
@@ -6371,5 +6371,80 @@ namespace IRAP.BL.MDM
                 WriteLog.Instance.WriteEndSplitter(strProcedureName);
             }
         }
+
+        /// <summary>
+        /// 获取未显示设备清单
+        /// </summary>
+        /// <param name="communityID">社区标识</param>
+        /// <param name="sysLogID">系统登录标识</param>
+        /// <param name="filterString">根据工位代码进行过滤</param>
+        /// <returns>List[WIPStationSPCMonitor]</returns>
+        public IRAPJsonResult ufn_GetList_NotOnStationEquipment(
+            int communityID,
+            long sysLogID,
+            out int errCode,
+            out string errText)
+        {
+            string strProcedureName =
+                string.Format(
+                    "{0}.{1}",
+                    className,
+                    MethodBase.GetCurrentMethod().Name);
+
+            WriteLog.Instance.WriteBeginSplitter(strProcedureName);
+            try
+            {
+                List<NotOnStationEquipment> datas = new List<NotOnStationEquipment>();
+
+                #region 创建数据库调用参数组，并赋值
+                IList<IDataParameter> paramList = new List<IDataParameter>();
+                paramList.Add(new IRAPProcParameter("@CommunityID", DbType.Int32, communityID));
+                paramList.Add(new IRAPProcParameter("@SysLogID", DbType.Int64, sysLogID));
+                WriteLog.Instance.Write(
+                    string.Format(
+                        "调用函数 IRAPMDM..ufn_GetList_NotOnStationEquipment，" +
+                        "参数：CommunityID={0}|SysLogID={1}",
+                        communityID,
+                        sysLogID),
+                    strProcedureName);
+                #endregion
+
+                #region 执行数据库函数或存储过程
+                try
+                {
+                    using (IRAPSQLConnection conn = new IRAPSQLConnection())
+                    {
+                        string strSQL =
+                                "SELECT * " +
+                                "FROM IRAPMDM..ufn_GetList_NotOnStationEquipment(" +
+                                "@CommunityID, @SysLogID) ORDER BY Ordinal";
+                                
+                        WriteLog.Instance.Write(strSQL, strProcedureName);
+
+                        IList<NotOnStationEquipment> lstDatas =
+                            conn.CallTableFunc<NotOnStationEquipment>(strSQL, paramList);
+                        datas = lstDatas.ToList();
+                        errCode = 0;
+                        errText = string.Format("调用成功！共获得 {0} 条记录", datas.Count);
+                        WriteLog.Instance.Write(errText, strProcedureName);
+                    }
+                }
+                catch (Exception error)
+                {
+                    errCode = 99000;
+                    errText = string.Format(
+                        "调用 IRAPMDM..ufn_GetList_NotOnStationEquipment 函数发生异常：{0}",
+                        error.Message);
+                    WriteLog.Instance.Write(errText, strProcedureName);
+                }
+                #endregion
+
+                return Json(datas);
+            }
+            finally
+            {
+                WriteLog.Instance.WriteEndSplitter(strProcedureName);
+            }
+        }
     }
 }
