d17e03e3e72b19fee1e3bf9ee3851e3b2594eeef
zhiying.li <zhiying.li@1351d8d5-24a7-d647-b81a-b5ab284c741b>
2018-06-04 15:33:37 +0000

    完善 PWOOutPutDetail Action类的功能

--- Sources/Client/GUI/IRAP.Client.GUI.MESPDC/Actions/PWOOutPutDetail.cs
+++ Sources/Client/GUI/IRAP.Client.GUI.MESPDC/Actions/PWOOutPutDetail.cs
@@ -7,27 +7,40 @@ using System.Text;
 using System.Windows.Forms;
 using System.Xml;
 
+using IRAP.Global;
+
 namespace IRAP.Client.GUI.MESPDC.Actions
 {
-    public class PWOOutPutDetailAction : CustomAction, IUDFAction
+    public class PWOOutputDetailAction : CustomAction, IUDFAction
     {
         private Scenario scene = Scenario.未设置;
-        private string IRAPCacheAddr = "";
-        private string PWONo = "";
-        XmlNode Param, Rows;
-        public PWOOutPutDetailAction(XmlNode actionParam, ExtendEventHandler extendAction, ref object tag)
+        private string irapCacheAddr = "";
+        private string pwoNo = "";
+        XmlNode nodeParam = null;
+        XmlNode nodeRows = null;
+        private long ut = 0;
+        private long actualTaktTime = 0;
+        private long pwoExecTime = 0;
+        private long actualOutput = 0;
+        private DateTime actualStartTime = DateTime.Now;
+
+        public PWOOutputDetailAction(XmlNode actionParam, ExtendEventHandler extendAction, ref object tag)
             : base(actionParam, extendAction, ref tag)
         {
             try
             {
-                Param = actionParam.ChildNodes[0] == null ? null : actionParam.ChildNodes[0];
-                Rows = actionParam.ChildNodes[1] == null ? null : actionParam.ChildNodes[1];
+                nodeParam = 
+                    actionParam.ChildNodes[0] == null ? null : actionParam.ChildNodes[0];
+                nodeRows = 
+                    actionParam.ChildNodes[1] == null ? null : actionParam.ChildNodes[1];
 
-                if (actionParam.Attributes["IRAPCacheAddr"] != null && !string.IsNullOrEmpty(actionParam.Attributes["IRAPCacheAddr"].Value))
+                if (actionParam.Attributes["IRAPCacheAddr"] != null && 
+                    !string.IsNullOrEmpty(actionParam.Attributes["IRAPCacheAddr"].Value))
                 {
-                    IRAPCacheAddr = actionParam.Attributes["IRAPCacheAddr"].Value;
+                    irapCacheAddr = actionParam.Attributes["IRAPCacheAddr"].Value;
                 }
-                if (actionParam.Attributes["Scenario"] != null && !string.IsNullOrEmpty(actionParam.Attributes["Scenario"].Value))
+                if (actionParam.Attributes["Scenario"] != null && 
+                    !string.IsNullOrEmpty(actionParam.Attributes["Scenario"].Value))
                 {
                     switch (actionParam.Attributes["Scenario"].Value)
                     {
@@ -48,9 +61,43 @@ namespace IRAP.Client.GUI.MESPDC.Actions
                             break;
                     }
                 }
-                if (actionParam.ChildNodes[0] != null && actionParam.ChildNodes[0].Attributes["PWONo"] != null && !string.IsNullOrEmpty(actionParam.ChildNodes[0].Attributes["PWONo"].Value))
+                if (actionParam.ChildNodes[0] != null && 
+                    actionParam.ChildNodes[0].Attributes["PWONo"] != null && 
+                    !string.IsNullOrEmpty(actionParam.ChildNodes[0].Attributes["PWONo"].Value))
                 {
-                    PWONo = actionParam.ChildNodes[0].Attributes["PWONo"].Value;
+                    pwoNo = actionParam.ChildNodes[0].Attributes["PWONo"].Value;
+                }
+
+                if (nodeParam != null)
+                {
+                    if (nodeParam.Attributes["UT"] != null)
+                    {
+                        ut = Tools.ConvertToInt64(nodeParam.Attributes["UT"].ToString());
+                    }
+                    if (nodeParam.Attributes["ActualTaktTime"] != null)
+                    {
+                        actualTaktTime =
+                            Tools.ConvertToInt64(
+                                nodeParam.Attributes["ActualTaktTime"].ToString());
+                    }
+                    if (nodeParam.Attributes["PWOExecTime"] != null)
+                    {
+                        pwoExecTime =
+                            Tools.ConvertToInt64(
+                                nodeParam.Attributes["PWOExecTime"].ToString());
+                    }
+                    if (nodeParam.Attributes["ActualOutput"] != null)
+                    {
+                        actualOutput =
+                            Tools.ConvertToInt64(
+                                nodeParam.Attributes["ActualOutput"].ToString());
+                    }
+                    if (nodeParam.Attributes["ActualStartTime"] != null)
+                    {
+                        actualStartTime =
+                            Tools.ConvertToDateTime(
+                                nodeParam.Attributes["ActualStartTime"].ToString());
+                    }
                 }
             }
             catch (Exception e)
@@ -61,7 +108,7 @@ namespace IRAP.Client.GUI.MESPDC.Actions
 
         public void DoAction()
         {
-            if (string.IsNullOrEmpty(IRAPCacheAddr))
+            if (string.IsNullOrEmpty(irapCacheAddr))
             {
                 throw new Exception("报文未设置Redis服务器地址");
             }
@@ -72,37 +119,37 @@ namespace IRAP.Client.GUI.MESPDC.Actions
                 case Scenario.获取工单产出报工明细:
                     throw new Exception("暂不支持获取工单产出报工明细");
                 case Scenario.WIPPattern报工推送入缓存:
-                    if (Param == null)
+                    if (nodeParam == null)
                     {
                         throw new Exception("报文缺少节点Param");
                     }
-                    if (string.IsNullOrEmpty(PWONo))
+                    if (string.IsNullOrEmpty(pwoNo))
                     {
                         throw new Exception("报文未设置工单号");
                     }
-                    ResponseContent res = GetListByPWONo(PWONo);
+                    ResponseContent res = GetListByPWONo(pwoNo);
                     if (res.RtnCode != 0 && res.RtnCode != 888)
                     {
                         throw new Exception(res.RtnMessage);
                     }
                     else
                     {
-                        if (res.DetailXml == null & res.DetailXml.ChildNodes.Count == 0)
+                        if (res.DetailXml == null && res.DetailXml.ChildNodes.Count == 0)
                         {
                             throw new Exception("缺少子板信息");
                         }
                         RequestContent request = new RequestContent();
-                        request.PWONo = PWONo;
+                        request.PWONo = pwoNo;
                         if (res.Param == null)
                         {
-                            request.Param = request.xdoc.ImportNode(Param, true);
+                            request.Param = request.xdoc.ImportNode(nodeParam, true);
                         }
                         else
                         {
                             request.Param = request.xdoc.ImportNode(res.Param, true);
                         }
                         int ordinal = 1;
-                        foreach (XmlNode r in Rows.ChildNodes)
+                        foreach (XmlNode r in nodeRows.ChildNodes)
                         {
                             int flag = 0;
                             foreach (XmlNode detail in res.DetailXml.ChildNodes)
@@ -135,6 +182,39 @@ namespace IRAP.Client.GUI.MESPDC.Actions
                             if (flag == 0)        //未找到
                             {
                                 XmlNode n = request.xdoc.ImportNode(r, true);
+
+                                actualOutput++;
+                                ut += actualTaktTime;
+                                pwoExecTime = Convert.ToInt64((DateTime.Now - actualStartTime).TotalMilliseconds);
+
+                                if (request.Param.Attributes["UT"] == null)
+                                {
+                                    request.Param.Attributes.Append(
+                                        request.xdoc.CreateAttribute("UT"));
+                                }
+                                request.Param.Attributes["UT"].Value = ut.ToString();
+                                if (request.Param.Attributes["ActualStartTime"] == null)
+                                {
+                                    request.Param.Attributes.Append(
+                                        request.xdoc.CreateAttribute("ActualStartTime"));
+                                }
+                                request.Param.Attributes["ActualStartTime"].Value = 
+                                    actualStartTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                                if (request.Param.Attributes["PWOExecTime"] == null)
+                                {
+                                    request.Param.Attributes.Append(
+                                        request.xdoc.CreateAttribute("PWOExecTime"));
+                                }
+                                request.Param.Attributes["PWOExecTime"].Value = 
+                                    pwoExecTime.ToString();
+                                if (request.Param.Attributes["ActualOutput"] == null)
+                                {
+                                    request.Param.Attributes.Append(
+                                        request.xdoc.CreateAttribute("ActualOutput"));
+                                }
+                                request.Param.Attributes["ActualOutput"].Value =
+                                    actualOutput.ToString();
+
                                 if (n.Attributes["Ordinal"] == null)
                                 {
                                     XmlAttribute a = request.xdoc.CreateAttribute("Ordinal");
@@ -149,13 +229,114 @@ namespace IRAP.Client.GUI.MESPDC.Actions
                     }
                     break;
                 case Scenario.从缓存中删除WIPPattern报工数据:
-                    throw new Exception("暂不支持从缓存中删除WIPPattern报工数据");
+                    if (nodeParam == null)
+                    {
+                        throw new Exception("报文缺少节点Param");
+                    }
+                    if (string.IsNullOrEmpty(pwoNo))
+                    {
+                        throw new Exception("报文未设置工单号");
+                    }
+                    ResponseContent responseContent = GetListByPWONo(pwoNo);
+                    if (responseContent.RtnCode != 0 && responseContent.RtnCode != 888)
+                    {
+                        throw new Exception(responseContent.RtnMessage);
+                    }
+                    else
+                    {
+                        if (responseContent.DetailXml == null && 
+                            responseContent.DetailXml.ChildNodes.Count == 0)
+                        {
+                            throw new Exception("缺少子板信息");
+                        }
+
+                        RequestContent request = new RequestContent();
+                        request.PWONo = pwoNo;
+                        if (responseContent.Param == null)
+                        {
+                            request.Param = request.xdoc.ImportNode(nodeParam, true);
+                        }
+                        else
+                        {
+                            request.Param = 
+                                request.xdoc.ImportNode(
+                                    responseContent.Param, 
+                                    true);
+                        }
+
+                        request.DetailXml =
+                            request.xdoc.ImportNode(
+                                responseContent.DetailXml,
+                                true);
+
+                        int ordinal = 1;
+                        foreach (XmlNode r in nodeRows.ChildNodes)
+                        {
+                            foreach (XmlNode detail in request.DetailXml.ChildNodes)
+                            {
+                                if (detail.Attributes["WIPCode"].Value == r.Attributes["WIPCode"].Value)
+                                {
+                                    request.xdoc.RemoveChild(detail);
+
+                                    actualOutput--;
+                                    ut -= actualTaktTime;
+                                    pwoExecTime = Convert.ToInt64((DateTime.Now - actualStartTime).TotalMilliseconds);
+
+                                    if (request.Param.Attributes["UT"] == null)
+                                    {
+                                        request.Param.Attributes.Append(
+                                            request.xdoc.CreateAttribute("UT"));
+                                    }
+                                    request.Param.Attributes["UT"].Value = ut.ToString();
+                                    if (request.Param.Attributes["ActualStartTime"] == null)
+                                    {
+                                        request.Param.Attributes.Append(
+                                            request.xdoc.CreateAttribute("ActualStartTime"));
+                                    }
+                                    request.Param.Attributes["ActualStartTime"].Value =
+                                        actualStartTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                                    if (request.Param.Attributes["PWOExecTime"] == null)
+                                    {
+                                        request.Param.Attributes.Append(
+                                            request.xdoc.CreateAttribute("PWOExecTime"));
+                                    }
+                                    request.Param.Attributes["PWOExecTime"].Value =
+                                        pwoExecTime.ToString();
+                                    if (request.Param.Attributes["ActualOutput"] == null)
+                                    {
+                                        request.Param.Attributes.Append(
+                                            request.xdoc.CreateAttribute("ActualOutput"));
+                                    }
+                                    request.Param.Attributes["ActualOutput"].Value =
+                                        actualOutput.ToString();
+
+                                    break;
+                                }
+                            }
+
+                            ordinal = 1;
+                            foreach (XmlNode row in request.DetailXml.ChildNodes)
+                            {
+                                if (row.Attributes["Ordinal"] == null)
+                                {
+                                    row.Attributes.Append(
+                                        request.xdoc.CreateAttribute("Ordinal"));
+                                }
+                                row.Attributes["Ordinal"].Value = ordinal.ToString();
+                                ordinal++;
+                            }
+                        }
+                        ResponseContent response = new ResponseContent(Post(request.GenerateRequestContent()));
+                        MessageBox.Show(response.RtnMessage);
+                    }
+                    break;
 
             }
         }
+
         private string Post(string input)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(IRAPCacheAddr);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(irapCacheAddr);
             request.Method = "POST";
 
             //request.ContentType = "application/xml;charset=UTF-8;";
@@ -185,23 +366,26 @@ namespace IRAP.Client.GUI.MESPDC.Actions
                 throw e;
             }
         }
+
         private ResponseContent GetListByPWONo(string PwoNo)
         {
 
-            string content = "<Softland><Redis><ExCode>GET</ExCode><Key>PWOInExecution=" + PWONo
-         + "</Key><Std>1</Std></Redis></Softland>";
+            string content = 
+                $"<Softland><Redis><ExCode>GET</ExCode><Key>PWOInExecution={pwoNo}"+
+                "</Key><Std>1</Std></Redis></Softland>";
             ResponseContent res = new ResponseContent(Post(content));
             return res;
         }
-
     }
-    public class PWOOutPutDetailFactory : CustomActionFactory, IUDFActionFactory
+
+    public class PWOOutputDetailFactory : CustomActionFactory, IUDFActionFactory
     {
         public IUDFAction CreateAction(XmlNode actionParams, ExtendEventHandler extendAction, ref object tag)
         {
-            return new PWOOutPutDetailAction(actionParams, extendAction, ref tag);
+            return new PWOOutputDetailAction(actionParams, extendAction, ref tag);
         }
     }
+
     public enum Scenario
     {
         未设置 = 0,
@@ -210,6 +394,7 @@ namespace IRAP.Client.GUI.MESPDC.Actions
         WIPPattern报工推送入缓存 = 3,
         从缓存中删除WIPPattern报工数据 = 4
     }
+
     public class RequestContent
     {
         public XmlDocument xdoc = new XmlDocument();
@@ -224,7 +409,9 @@ namespace IRAP.Client.GUI.MESPDC.Actions
         {
             try
             {
-                string request = $"<Softland><Redis><ExCode>SET</ExCode><Key>PWOInExecution={PWONo}</Key><Value><Parameters></Parameters></Value></Redis></Softland>";
+                string request =
+                    $"<Softland><Redis><ExCode>SET</ExCode><Key>PWOInExecution={PWONo}" +
+                    "</Key><Value><Parameters></Parameters></Value></Redis></Softland>";
                 xdoc.LoadXml(request);
                 xdoc.SelectNodes("/Softland/Redis/Value/Parameters")[0].AppendChild(Param);
                 xdoc.SelectNodes("/Softland/Redis/Value/Parameters")[0].AppendChild(DetailXml);
@@ -236,6 +423,7 @@ namespace IRAP.Client.GUI.MESPDC.Actions
             }
         }
     }
+
     public class ResponseContent
     {
         public XmlDocument xdoc = new XmlDocument();
