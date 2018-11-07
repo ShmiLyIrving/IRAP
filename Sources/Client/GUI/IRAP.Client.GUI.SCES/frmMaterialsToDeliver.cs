a7bd1485cfce34ba75ee7b8d7ea891cf74ec7b22
zhiying.li <zhiying.li@1351d8d5-24a7-d647-b81a-b5ab284c741b>
2018-11-07 06:29:26 +0000

    去除了在物料配送功能界面中，对60030社区部分产线进行“配送数量”修改的权限限制。

--- Sources/Client/GUI/IRAP.Client.GUI.SCES/frmMaterialsToDeliver.cs
+++ Sources/Client/GUI/IRAP.Client.GUI.SCES/frmMaterialsToDeliver.cs
@@ -237,7 +237,7 @@ namespace IRAP.Client.GUI.SCES
                                             btnActualQtyToDeliverModify.Visible = true;
                                             break;
                                         default:
-                                            btnActualQtyToDeliverModify.Visible = false;
+                                            btnActualQtyToDeliverModify.Visible = true;
                                             break;
                                     }
 
